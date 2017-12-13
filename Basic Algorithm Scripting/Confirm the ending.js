a08f650790a2bfe966a2197a984e18a12503374d
Kimberly <kowens4@elon.edu>
2017-12-13 15:17:40 -0500

    wip

---
+++ Basic Algorithm Scripting/Confirm the ending.js
@@ -0,0 +1,12 @@
+///WIP
+function confirmEnding(str, target) {
+    var len =  target.length;
+    console.log('this is a number',len);
+    var test = str.substr(len);
+    console.log('here i am', test);
+
+    return str;
+
+}
+
+confirmEnding("Bastian", "n");
