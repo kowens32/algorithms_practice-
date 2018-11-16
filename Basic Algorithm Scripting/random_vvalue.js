8b8f1d37580e0bc21962debf855851413ed964b6
Kimberly <kowens4@elon.edu>
2018-11-16 18:15:28 -0500

    adding a page

---
+++ Basic Algorithm Scripting/random_vvalue.js
@@ -0,0 +1,9 @@
+var stuff [10,34,56,67,93,120,137,168,259,280,311,413,524];
+var random_value = stuff[Math.floor(Math.random() *14)];
+
+for (i = 0; i < stuff.length; i++) {
+    if (
+    stuff == random_value
+    )
+        alert('something!');
+}
